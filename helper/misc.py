418cff467a8c751124d1016749b0a5ad3b501df6
LORD_CODE <lordgrief176@gmail.com>
2022-04-15 20:44:26 +0300

    ModulesDict

--- helper/misc.py
+++ helper/misc.py
@@ -1,5 +1,22 @@
 from git import Repo
 
+
+class ModulesDict(dict):
+    def __init__(self, **items):
+        super().__init__(items)
+        self.__dict = None
+
+    def __setitem__(self, module: str, value: dict):
+        if module in self.__dict:
+            raise KeyError(f"Module {module} already exists")
+        self.__dict[module] = value
+
+    def __getitem__(self, module: str):
+        if module not in self.__dict:
+            raise KeyError(f"Module {module} does not exist")
+        return self.__dict[module]
+
+
 git = Repo(".")
 commits_from_repo = list(git.iter_commits())
 result = len(commits_from_repo)
