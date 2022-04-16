5a56f53fd6766836f8f2fccbaff67461c7ee55dc
LORD_CODE <lordgrief176@gmail.com>
2022-04-16 13:38:37 +0300

    Modules

--- modules/ping.py
+++ modules/ping.py
@@ -6,6 +6,6 @@ from helper.module import module
 @module(commands=['ping', 'p'])
 async def ping_cmd(_, message: Message):
     start = perf_counter()
-    await message.edit("<b>☻ Pong!</b>")
+    await message.edit("<b>○ Pong!</b>")
     end = round((perf_counter() - start) * 1000)
-    await message.edit(f"<b>☺ Pong! {end}ms</b>")
+    await message.edit(f"<b>● Pong! {end}ms</b>")
