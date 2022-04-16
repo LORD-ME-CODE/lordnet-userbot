5bcbe1a3ff46d46f14e4db6b2c15969d72c6362c
LORD_CODE <lordgrief176@gmail.com>
2022-04-16 21:46:30 +0300

    spam module

---
+++ modules/spam.py
@@ -0,0 +1,34 @@
+from asyncio import sleep
+
+from helper import module, Message, prefix
+
+
+cooldowns = {"spam": 0.15, "fastspam": 0}
+
+
+@module(commands=["spam", "fastspam"], args=["times", "text"])
+async def spam_cmd(_, message: Message):
+    cmd = message.command[0]
+
+    if len(message.command) < 3:
+        await message.reply(
+            "<b>🙂 Usage:</b> <code>{}{}</code> <code>[times] [text]</code>".format(
+                prefix(), cmd
+            )
+        )
+        return
+
+    await message.delete()
+
+    cooldown = cooldowns[cmd]
+
+    text = message.text.split(maxsplit=1)[1]
+    reply = message.reply_to_message.message_id if message.reply_to_message else None
+
+    if cooldown:
+        for _ in range(int(message.command[1])):
+            await message.reply(text=text, reply_to_message_id=reply)
+            await sleep(cooldown)
+    else:
+        for _ in range(int(message.command[1])):
+            await message.reply(text=text, reply_to_message_id=reply)
