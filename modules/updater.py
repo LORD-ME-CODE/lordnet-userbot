5912e09ed402dd601d1cde17828b68adfd2afd26
lordcodes <83734728+LORD-ME-CODE@users.noreply.github.com>
2022-04-16 23:57:21 +0300

    Update updater.py

--- modules/updater.py
+++ modules/updater.py
@@ -31,15 +31,15 @@ async def restart_cmd(_, message: Message):
 @module(commands=["update"], desc="Update the lordnet-userbot")
 async def update_cmd(_, message: Message):
     try:
-        await message.edit("<b>✇ Updating pip...</b>")
+        await message.edit("<b>✇ Updating pip...</b> (<code>25%</code>)")
         subprocess.run([sys.executable, "-m", "pip", "install", "-U", "pip"])
-        await message.edit("<b>✇ Updating setuptools and wheel...</b>")
+        await message.edit("<b>✇ Updating setuptools and wheel...</b> (<code>50%</code>)")
         subprocess.run(
             [sys.executable, "-m", "pip", "install", "-U", "setuptools", "wheel"]
         )
-        await message.edit("<b>✇ Updating userbot from git...</b>")
+        await message.edit("<b>✇ Updating userbot from git...</b> (<code>75%</code>)")
         subprocess.run(["git", "pull"])
-        await message.edit("<b>✇ Updating from requirements.txt<code>...</code></b>")
+        await message.edit("<b>✇ Updating from requirements.txt<code>...</code> (<code>95%</code>)</b>")
         subprocess.run(
             [sys.executable, "-m", "pip", "install", "-U", "-r", "requirements.txt"]
         )
