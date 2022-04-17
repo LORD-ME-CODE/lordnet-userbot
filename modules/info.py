0b059854a296a27b4cf77b4942992ff9db02d206
lordcodes <83734728+LORD-ME-CODE@users.noreply.github.com>
2022-04-17 23:58:21 +0300

    Update info.py

--- modules/info.py
+++ modules/info.py
@@ -1,6 +1,7 @@
 from pyrogram.types import Message
 
 from helper.misc import python_version, __version__, modules_dict
+from helper.misc import build_version, build_link, c_date
 from helper.module import module
 
 
@@ -12,7 +13,8 @@ async def info(_, message: Message):
 
     text = (
         '<b><a href="https://t.me/lordnet_userbot">✉ lordnet-userbot</a> ИНФО:\n\n'
-        f"☛ lordnet версия: <code>{__version__}</code>\n\n"
+        f"☛ lordnet версия: <code>{__version__}</code>\n"
+        f"☛ Билд: <a href="{build_link}">{build_version}</a> ({c_date})\n\n"
         f"☛ Python версия: <code>{python_version}</code>\n"
         f"☛ Кол-во модулей: <code>{len(modules_dict)}</code>\n"
         f"☛ Лицензия: <a href='https://github.com/LORD-ME-CODE/lordnet-userbot/blob/main/LICENSE'>GNU v3.0</a>\n"
