fa1e794dfb01907ab47ae7f35139e83f91b0695f
lordcodes <83734728+LORD-ME-CODE@users.noreply.github.com>
2022-04-18 00:05:38 +0300

    Update info.py
    Black code

--- modules/info.py
+++ modules/info.py
@@ -14,7 +14,7 @@ async def info(_, message: Message):
     text = (
         '<b><a href="https://t.me/lordnet_userbot">✉ lordnet-userbot</a> ИНФО:\n\n'
         f"☛ lordnet версия: <code>{__version__}</code>\n"
-        f"☛ Билд: <a href="{build_link}">{build_version}</a> ({c_date})\n\n"
+        f"☛ Билд: <a href='{build_link}'>{build_version}</a> ({c_date})\n\n"
         f"☛ Python версия: <code>{python_version}</code>\n"
         f"☛ Кол-во модулей: <code>{len(modules_dict)}</code>\n"
         f"☛ Лицензия: <a href='https://github.com/LORD-ME-CODE/lordnet-userbot/blob/main/LICENSE'>GNU v3.0</a>\n"
