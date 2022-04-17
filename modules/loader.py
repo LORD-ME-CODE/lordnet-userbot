011f322380a950b8c9fbf0218521022ee78aa6e7
LORD_CODE <lordgrief176@gmail.com>
2022-04-17 12:38:10 +0300

    loader

--- modules/loader.py
+++ modules/loader.py
@@ -15,15 +15,17 @@ from validators import url
 # noinspection PyShadowingBuiltins
 from aiofile import async_open as open
 
+from helper.misc import lordnet_url
+
 
 @module(
-    cmds=["load", "unload", "lm", "ulm", "loadmod", "unloadmod"],
+    cmds=["load", "unload", "lm", "um", "updatemod"],
     desc="Load/unload modules",
-    args=["name"],
+    args=["name/link"],
 )
 async def loader_cmd(_, message: Message):
     cmd = message.command[0]
-    if cmd in ["load", "lm", "loadmod"]:
+    if cmd in ["load", "lm"]:
         if len(message.command) == 1:
             await message.edit("<b>🙄 Please specify a module to load</b>")
             return
@@ -36,7 +38,7 @@ async def loader_cmd(_, message: Message):
         if modules_dict.module_in(name):
             await message.edit(
                 f"<b>🙄 Module <code>{name}</code> already loaded\n"
-                f"🔃 Type <code>{prefix()}updatemod</code> to update it</b>"
+                f"🔃 Type <code>{prefix()}updatemod {name}</code> to update it</b>"
             )
             return
 
@@ -44,6 +46,7 @@ async def loader_cmd(_, message: Message):
             if not await module_exists(name):
                 await message.edit(f"<b>🙄 Module <code>{name}</code> does not exist\n")
                 return
+
         else:
             link = message.command[1]
             async with session.get(link) as response:
@@ -54,15 +57,56 @@ async def loader_cmd(_, message: Message):
                     )
                     return
                 data = await response.read()
+                if b"from helper import " not in data:
+                    return await message.edit(
+                        f"<b>🙄 Module <code>{name}</code> is not a valid module\n"
+                        f"🔃 Check the link and try again</b>"
+                    )
                 async with open(f"custom/{name}.py", "wb") as f:
                     await f.write(data)
                 await load_module(name + ".py")
         await message.edit(f"<b>💪 Module <code>{name}</code> loaded</b>")
+    elif cmd == "updatemod":
+        if len(message.command) == 1:
+            await message.edit("<b>🙄 Please specify a module to update</b>")
+            return
+        name = message.command[1].lower()
+        if url(name):
+            name = message.command[1].split("/")[-1].replace(".py", "")
+            is_url = True
+        else:
+            is_url = False
+        if not modules_dict.module_in(name):
+            await message.edit(
+                f"<b>🙄 Module <code>{name}</code> not loaded\n"
+                f"🔃 Type <code>{prefix()}lm {message.command[1].lower()}</code> to load it</b>"
+            )
+            return
+        if not is_url:
+            link = lordnet_url + f"modules/{name}.py"
+        else:
+            link = message.command[1]
+        async with session.get(link) as response:
+            if response.status != 200:
+                await message.edit(
+                    f"<b>🙄 Module <code>{name}</code> does not exist\n"
+                    f"🔃 Check the link and try again</b>"
+                )
+                return
+            data = await response.read()
+            if is_url and b"from helper import " not in data:
+                return await message.edit(
+                    f"<b>🙄 Module <code>{name}</code> is not a valid module\n"
+                    f"🔃 Check the link and try again</b>"
+                )
+            async with open(f"custom/{name}.py", "wb") as f:
+                await f.write(data)
+            await load_module(name + ".py")
     else:
         if len(message.command) == 1:
             await message.edit("<b>🙄 Please specify a module to unload</b>")
             return
-        name = message.command[1].lower().split("/")[-1].replace(".py", "")
+        name = message.command[1].split("/")[-1].replace(".py", "")
         if name + ".py" not in os.listdir("custom"):
             await message.edit(f"<b>🙂 Module <code>{name}</code> not found.</b>")
             return
