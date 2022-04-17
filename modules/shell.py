72bcdf4b16eb76dee561f544a69867dfc78bccfe
LORD_CODE <lordgrief176@gmail.com>
2022-04-17 20:48:01 +0300

    shell input

--- modules/shell.py
+++ modules/shell.py
@@ -36,3 +36,44 @@ async def shell_cmd(_: Client, message: Message):
         text += f"<b>Выполнено за {round(stop_time - start_time, 5)} сек. | Код ({cmd_obj.returncode})</b>"
     await message.edit(text)
     cmd_obj.kill()
+
+
+@module(
+    cmds=["shinput", "shin"],
+    args=["команда", "input={текст}"],
+    desc="Выполнить консольную команду с вводом",
+)
+async def shell_input_cmd(_: Client, message: Message):
+    if len(message.command) < 2:
+        return await message.edit("<b>✖ Вы не указали команду.</b>")
+    cmd_text = message.text.split(maxsplit=1)[1]
+    try:
+        inp = cmd_text.split("input=", maxsplit=1)[1]
+        cmd_text = cmd_text.split("input=" + inp, maxsplit=1)[0]
+    except IndexError:
+        return await message.edit("<b>✖ Вы не указали входной текст.</b>")
+    cmd_obj = Popen(
+        cmd_text,
+        shell=True,
+        stdout=PIPE,
+        stderr=PIPE,
+        text=True,
+    )
+    text = f"<b>≻</b> <code>{cmd_text}</code>\n\n"
+
+    await message.edit(text + "<b>Выполняю...</b>")
+    try:
+        start_time = perf_counter()
+        print(inp)
+        stdout, stderr = cmd_obj.communicate(input=inp)
+    except TimeoutExpired:
+        text += "<b>✖ Таймаут (60 сек)</b>"
+    else:
+        stop_time = perf_counter()
+        if stdout:
+            text += "<b>⌬ Вывод:</b>\n" f"<code>{stdout}</code>\n\n"
+        if stderr:
+            text += "<b>✖ Ошибка:</b>\n" f"<code>{stderr}</code>\n\n"
+        text += f"<b>Выполнено за {round(stop_time - start_time, 5)} сек. | Код ({cmd_obj.returncode})</b>"
+    await message.edit(text)
+    cmd_obj.kill()
