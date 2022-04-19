2c6d5c4d7f66f1368bac9e860b150e8fa31649c1
LORD_CODE <lordgrief176@gmail.com>
2022-04-19 21:21:36 +0300

    [+] ping.py update

--- modules/ping.py
+++ modules/ping.py
@@ -1,12 +1,12 @@
-from time import perf_counter
+from time import perf_counter_ns
 from pyrogram.types import Message
 from helper.module import module
 
 
 @module(commands=["ping", "p"], description="Проверить пинг")
 async def ping_cmd(_, message: Message):
-    start = perf_counter()
+    start = perf_counter_ns()
     await message.edit("<b>○ Понг!</b>")
-    end = perf_counter()
+    end = perf_counter_ns()
     res = round((end - start) * 0.000001, 3)
     await message.edit(f"<b>● Понг! {res}мс</b>")
