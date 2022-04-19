9d93218259d8cd8ab47a06384269a68fa32d92e5
lordcodes <83734728+LORD-ME-CODE@users.noreply.github.com>
2022-04-20 01:20:14 +0300

    Update shell.py
    Сложно без тестов

--- modules/shell.py
+++ modules/shell.py
@@ -14,9 +14,9 @@ async def shell_cmd(_: Client, message: Message):
     if len(message.command) < 2:
         return await message.edit("<b>✖ Вы не указали команду.</b>")
     cmd_text = message.text.split(maxsplit=1)[1]
-    cmd_obj = await asyncio.create_subprocess_exec(
+    cmd_obj = await asyncio.create_subprocess_shell(
         cmd_text,
-        
+        shell=True,
         stdout=PIPE,
         stderr=PIPE,
         text=True,
@@ -54,9 +54,9 @@ async def shell_input_cmd(_: Client, message: Message):
         cmd_text = cmd_text.split("input=" + inp, maxsplit=1)[0]
     except IndexError:
         return await message.edit("<b>✖ Вы не указали входной текст.</b>")
-    cmd_obj = await asyncio.create_subprocess_exec(
+    cmd_obj = await asyncio.create_subprocess_shell(
         cmd_text,
-        
+        shell=True,
         stdout=PIPE,
         stderr=PIPE,
         stdin=PIPE,
