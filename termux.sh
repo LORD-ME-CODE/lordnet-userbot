30d3f029fcb5aedf023940712b84e14a61011edf
lordcodes <83734728+LORD-ME-CODE@users.noreply.github.com>
2022-04-20 00:55:27 +0300

    Update termux.sh

--- termux.sh
+++ termux.sh
@@ -6,7 +6,7 @@ fi
 termux-wake-lock
 
 apt update -y
-apt install python3 git clang wget libjpeg-turbo libcrypt ndk-sysroot zlib -y || exit 2
+apt install python3 git clang wget python3-opencv libjpeg-turbo libcrypt ndk-sysroot zlib -y || exit 2
 
 python3 -m pip install -U pip
 LDFLAGS="-L${PREFIX}/lib/" CFLAGS="-I${PREFIX}/include/" pip3 install --upgrade wheel pillow
