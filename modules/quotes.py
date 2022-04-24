532bd98ba38effa319d4820feb1e3ea96d7fd1ed
lordcodes <83734728+LORD-ME-CODE@users.noreply.github.com>
2022-04-25 00:33:39 +0300

    Update quotes.py
    Quotes?

--- modules/quotes.py
+++ modules/quotes.py
@@ -30,7 +30,8 @@ async def quote_cmd(client: Client, message: types.Message):
     msgs = [
         msg
         async for msg in client.get_chat_history(
-            message.chat.id, offset_id=message.reply_to_message.id,
+            message.chat.id,
+            offset_id=message.reply_to_message.id,
             limit=count,
         )
     ][::-1]
@@ -204,7 +205,7 @@ async def render_message(app: Client, message: types.Message) -> dict:
                 {
                     "offset": entity.offset,
                     "length": entity.length,
-                    "type": entity.type,
+                    "type": str(entity.type),
                 }
             )
 
