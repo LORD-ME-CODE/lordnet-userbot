dcc9369f14755d562a5ba250aac0ef750866255c
LORD_CODE <lordgrief176@gmail.com>
2022-04-28 11:19:46 +0300

    GNU V3

--- helper/misc.py
+++ helper/misc.py
@@ -1,3 +1,16 @@
+#
+#  _    ___  ___ ___  _  _ ___ _____    _   _ ___ ___ ___ ___  ___ _____
+# | |  / _ \| _ \   \| \| | __|_   _|__| | | / __| __| _ \ _ )/ _ \_   _|
+# | |_| (_) |   / |) | .` | _|  | ||___| |_| \__ \ _||   / _ \ (_) || |
+# |____\___/|_|_\___/|_|\_|___| |_|     \___/|___/___|_|_\___/\___/ |_|
+#
+#                            © Copyright 2022
+#
+#                       https://t.me/lordnet_userbot
+#
+# 🔒 Licensed under the GNU GPLv3
+# 🌐 https://www.gnu.org/licenses/agpl-3.0.html
+
 from __future__ import annotations
 
 from sys import version_info
