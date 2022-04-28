4331fe2849ecd84741db650eb3a9228f88abc584
LORD_CODE <lordgrief176@gmail.com>
2022-04-28 11:19:13 +0300

    GNU V3

--- helper/__init__.py
+++ helper/__init__.py
@@ -1,3 +1,16 @@
+#
+#  _    ___  ___ ___  _  _ ___ _____    _   _ ___ ___ ___ ___  ___ _____
+# | |  / _ \| _ \   \| \| | __|_   _|__| | | / __| __| _ \ _ )/ _ \_   _|
+# | |_| (_) |   / |) | .` | _|  | ||___| |_| \__ \ _||   / _ \ (_) || |
+# |____\___/|_|_\___/|_|\_|___| |_|     \___/|___/___|_|_\___/\___/ |_|
+#
+#                            © Copyright 2022
+#
+#                       https://t.me/lordnet_userbot
+#
+# 🔒 Licensed under the GNU GPLv3
+# 🌐 https://www.gnu.org/licenses/agpl-3.0.html
+
 from .module import module, modules_dict, module_exists, escape_html
 from .cmd import restart, import_library, aimport_library, exception_str
 from .misc import python_version, prefix, __version__, session
